ad96a71eb95f50aeee97195262289abb5707b1c4
Allanvini <allanvinisilva@gmail.com>
2021-08-13 15:50:06 -0300

    alterado o design dos botoes de pesquisa

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -2,7 +2,7 @@ import styles from "./menu.module.css";
 import MenuButton from "./menuButton/menuButton";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import Speech from 'react-speech';
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function Menu() {
 
@@ -19,31 +19,36 @@ export default function Menu() {
     if (!browserSupportsSpeechRecognition) {
         console.log("Browser do not support speech recognition")
     }
-    
+
     return (
         <nav className={styles.navbar}>
             <img className={styles.navlogo} src="https://store.cloudflare.steamstatic.com/public/shared/images/header/logo_steam.svg?t=962016" width="190" />
             <div className={styles.searchBar}>
-                
-                <input value={searchString} onChange={(event)=>{setSearchString(event.target.value)}} placeHolder="digite sua pesquisa" />
 
-                <Speech text={searchString}>Play</Speech>
+                <input value={searchString} onChange={(event) => { setSearchString(event.target.value) }} placeHolder="digite sua pesquisa" />
 
-                <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} className={`h-6 w-6 ${styles.searchBarIcon}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
 
-                <svg onClick={()=>{
-                    if(!isRecording){
+
+                <button className={styles.searchBarIcon}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} className={'h-6 w-6'} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+
+                <button className={styles.searchBarIcon} onClick={() => {
+                    if (!isRecording) {
                         setIsRecording(true);
                         return SpeechRecognition.startListening();
                     }
                     setIsRecording(false);
                     SpeechRecognition.stopListening();
                     return setSearchString(transcript);
-                }} xmlns="http://www.w3.org/2000/svg" width={25} height={25} className={`h-5 w-5 ${styles.searchBarIcon}`} viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" />
-                </svg>
+                }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} className={'h-5 w-5'} viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" />
+                    </svg>
+                </button>
+
 
             </div>
             <div className={styles.navlink}>
