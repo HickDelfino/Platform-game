d31f8687594dcd82c2d886a72007b2360592caf7
allanvini <allanvinisilva@gmail.com>
2021-08-12 23:50:28 -0300

    implementada conversão de texto em fala

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,6 +1,7 @@
 import styles from "./menu.module.css";
 import MenuButton from "./menuButton/menuButton";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import Speech from 'react-speech';
 import {useState} from 'react';
 
 export default function Menu() {
@@ -25,6 +26,8 @@ export default function Menu() {
             <div className={styles.searchBar}>
                 
                 <input value={searchString} onChange={(event)=>{setSearchString(event.target.value)}} placeHolder="digite sua pesquisa" />
+
+                <Speech text={searchString}>Play</Speech>
 
                 <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} className={`h-6 w-6 ${styles.searchBarIcon}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
