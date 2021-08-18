b5a18b350445f88a85917352915304540849fa80
Allanvini <allanvinisilva@gmail.com>
2021-08-18 15:52:54 -0300

    iniciado desenvolvimento da pagina do jogo

--- src/components/cardmain/cardButton/cardButton.js
+++ src/components/cardmain/cardButton/cardButton.js
@@ -1,5 +1,6 @@
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
     root: {
@@ -7,7 +8,7 @@ const useStyles = makeStyles({
         backgroundColor: 'white',
         color: 'blue',
         textTransform: 'none',
-        width: '50%',
+        width: '100%',
         margin: '5px',
         height: '45px',
         "&:hover": {
@@ -18,13 +19,15 @@ const useStyles = makeStyles({
     }
 })
 
-export default function CardButton({ children, gameData }) {
+export default function CardButton({ children, gameData, href }) {
 
     const classes = useStyles();
 
     return (
-        <Button classes={classes}>
-            {children}
-        </Button>
+        <Link to={href} style={{textDecoration: 'none', width: '45%', margin: "5px", marginLeft: '-5px', padding: 'none'}}>
+            <Button classes={classes} href={href}>
+                {children}
+            </Button>
+        </Link>
     )
 }
--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -1,15 +1,33 @@
 import styles from './game.module.css';
 import { useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function Game(){
+import Container from '../../components/container/container';
 
-    const params = useParams().id; //Pega o parametro id da URL definido no App.js
+export default function Game() {
+    const id = useParams().id;
+    const [game, setGame] = useState({});
+
+    useEffect(async () => {
+        await axios.get(`http://localhost:4001/api/games/selected/${id}`).then((response) => {
+            setGame(response.data);
+        }).catch((err) => {
+
+        })
+    }, []);
 
     return (
-        <div style={{marginTop: '100px'}}>
-            ID: {params}
-        </div>
+        <Container>
+            <div className={styles.pageContent}>
+                <div className={styles.gameData}>
+                    <img src={game.coverImage} className={styles.gameDataCoverImage} />
+                </div>
+                <div className={styles.gameDetailsAndReviews}>
+                    <h1>{game.gameTitle}</h1>
+                </div>
+            </div>
+        </Container>
+
     )
 }
-
-// https://www.youtube.com/watch?v=y_pr4lRoUto
